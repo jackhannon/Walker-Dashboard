8e961f37d6742efbe6267f1eeadfcf4d10255da8
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-08-27 16:34:59 -0700

    fixed cors

--- backend/config/allowedOrigins.ts
+++ backend/config/allowedOrigins.ts
@@ -1,4 +1,7 @@
+import envConfig from "./envConfig.js";
+
 const allowedOrigins = [
+  envConfig.CLIENT_DOMAIN,
   'http://127.0.0.1:5500',
   'http://localhost:5173',
   'http://localhost:5174',
--- backend/config/corsOptions.ts
+++ backend/config/corsOptions.ts
@@ -2,7 +2,6 @@ import { allowedOrigins } from "./allowedOrigins";
 // 
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    //short circuiting garauntees this will be a string if it gets to the 2nd half of the eval
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
