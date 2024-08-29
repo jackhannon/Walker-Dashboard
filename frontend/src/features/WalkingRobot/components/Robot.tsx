fd5049bbfd323318b206ec6dd03eb7cd6f89c010
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-08-29 15:33:14 -0700

    fixed type errors

--- frontend/src/features/WalkingRobot/components/Robot.tsx
+++ frontend/src/features/WalkingRobot/components/Robot.tsx
@@ -14,7 +14,7 @@ const Robot: React.FC<Props> = ({frame}) => {
       <Hull coordinate={frame.hull.coordinate} angle={frame.hull.angle}/>
       <UpperLeg coordinate={frame.leftLeg.hip.coordinate} angle={frame.leftLeg.hip.angle}/>
       <LowerLeg coordinate={frame.leftLeg.knee.coordinate} angle={frame.leftLeg.knee.angle}/>
-
+  
       <UpperLeg coordinate={frame.rightLeg.hip.coordinate} angle={frame.rightLeg.hip.angle}/>
       <LowerLeg coordinate={frame.rightLeg.knee.coordinate} angle={frame.rightLeg.knee.angle}/>
     </>
