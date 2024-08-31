c5e767e3a0541d438a3edc1d0cca36b172ddc3a9
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-08-31 10:44:03 -0700

    refactored for provider setup

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,5 +1,9 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL || "http://localhost:4000", )
+const role = "client"; // or "frame-provider" based on the role
 
-export {socket};
+const socket = io(import.meta.env.VITE_API_URL || "http://localhost:4000", {
+  query: { role }
+});
+
+export { socket };
