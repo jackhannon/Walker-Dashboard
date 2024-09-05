12e45b38d4bace908e897189c1e5833b5e30ac45
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-09-05 09:27:56 -0700

    change cors

--- backend/config/corsOptions.ts
+++ backend/config/corsOptions.ts
@@ -2,11 +2,11 @@ import { allowedOrigins } from "./allowedOrigins";
 
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    // if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    // } else {
+    //   callback(new Error("Not allowed by CORS"));
+    // }
   },
   optionsSuccessStatus: 200,
 };
